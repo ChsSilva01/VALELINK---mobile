d715632277ba7d47d52cc434642861acfd07847a
CarlosDosGit <chsilva1419@gmail.com>
2024-04-28 23:10:07 -0300

    vale link v1.7

--- ValeLINK/src/routes/index.js
+++ ValeLINK/src/routes/index.js
@@ -1,6 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import DrawerRoutes from './drawer.routes.js';
-import AuthStack from './login.routes.js';
+import LoginScreen from './login.routes.js';
 
 const userLoggedIn = false;
 export default function Routes(){
@@ -10,8 +10,9 @@ export default function Routes(){
              {userLoggedIn ? (
                 <DrawerRoutes />
             ) : (
-                <AuthStack />
+                <LoginScreen />
             )}
         </NavigationContainer>
     )
 }
+//Aqui eu deixo bagulhado eu tiro esse Drawer ae, e coloco no login para quando clicar ir para esse lugar. bruh sei la
--- ValeLINK/src/screens/Empresas/index.js
+++ ValeLINK/src/screens/Empresas/index.js
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, View, TouchableOpacity, Image, TextInput, ImageBackground } from 'react-native';
 
-export default function Empresas(){
+export default function Empresas({ Navigation }){
     return(
         <Text>Empresas</Text>
     )
