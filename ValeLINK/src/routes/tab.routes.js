53351084a5295b70343024649f34343aaa4a8b9b
3º ETIM - Desenvolvimento de Sistemas <ds-etim-3@servidor.acad>
2024-05-24 11:57:41 -0300

    valelink v4.1

--- ValeLINK/src/routes/tab.routes.js
+++ ValeLINK/src/routes/tab.routes.js
@@ -62,30 +62,8 @@ const TabRoutes = ({ navigation}) => {
     >
       {/* Cada tab direciona para a página que eu quiser' */}
       <Tab.Screen name= "Home" component={Home} options={{tabBarLabel: ''}}></Tab.Screen>
-      <Tab.Screen
-    name="Pesquisar"
-    options={{
-        tabBarLabel: '',
-        tabBarButton: (props) => (
-            <TouchableOpacity
-                {...props}
-                style={{
-                    flex: 1,
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                }}
-                onPress={() => {
-                    navigation.navigate('Pesquisar');
-                }}
-            >
-                <Ionicons name="search-outline" color={'#fff'} size={35} />
-            </TouchableOpacity>
-        ),
-    }}
->
-    {() => null}
-</Tab.Screen>
-      <Tab.Screen name= "Empresas" component={Empresas} options={{tabBarLabel: '',tabBarStyle:{marginStart: 10}}}></Tab.Screen>
+      <Tab.Screen name= "Pesquisar" component={Pesquisar} options={{tabBarLabel: ''}}></Tab.Screen>
+      <Tab.Screen name= "Empresas" component={Empresas} options={{tabBarLabel: ''}}></Tab.Screen>
       <Tab.Screen name= "Notificações" component={Notificacoes} options={{tabBarLabel: ''}}></Tab.Screen>
     </Tab.Navigator>
     )
